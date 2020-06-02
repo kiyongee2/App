849112fff463ded5c9d2cbeaacae79576d0be43a
kiyongee2 <kiyongee2@gmail.com>
2020-06-02 20:22:25 +0900

    BufferedReader

--- TypingGame.java
+++ TypingGame.java
@@ -1,35 +1,53 @@
-package math_random;
+package exercise;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class TypingGame {
 
 	public static void main(String[] args) {
-		String[] data = {"bear", "cat", "dog", "eagle", "monkey", "tiger"
-				         , "penguin", "lion", "snake", "dolphin"};
-		int n = 1;    //문제 번호
-		long millisecond = 0;
-		
-		System.out.println("영어타자 게임");
-		Scanner sc = new Scanner(System.in);
-		millisecond = System.currentTimeMillis();    //게임 시작 전 시간
-		for(int i=0; i<10; i++) {
-			int rand = (int)(Math.random()*data.length);
-			System.out.println("문제" + n);
-			String question = data[rand];
-			System.out.println(question);
+		try(FileReader fr = new FileReader("words.txt");
+			BufferedReader br = new BufferedReader(fr)){
+			String line;
+			String[] words=null;
+			while((line=br.readLine()) !=null) {
+				words = line.split(" ");
+			}
 			
-			String answer = sc.next();
-			if(answer.equals(question)) {
-				System.out.println("통과!");
-				n++;
+			int n = 1;    //문제 번호
+			br.close();
+			long millisecond = 0;
+			
+			System.out.println("영어타자 게임");
+			Scanner sc = new Scanner(System.in);
+			millisecond = System.currentTimeMillis();    //게임 시작 전 시간
+			for(int i=0; i<10; i++) {
+				int rand = (int)(Math.random()*words.length);
+				System.out.println("문제" + n);
+				String question = words[rand];
+				System.out.println(question);
+				
+				String answer = sc.next();
+				if(answer.equals(question)) {
+					System.out.println("통과!");
+					n++;
+				}
+				else {
+					System.out.println("오타! 다시 도전");
+				}
 			}
-			else {
-				System.out.println("오타! 다시 도전");
-			}
+			millisecond = System.currentTimeMillis() - millisecond;
+			System.out.println("게임에 걸린 시간은 " + (float)millisecond/1000+ "초입니다.");
+			sc.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-		millisecond = System.currentTimeMillis() - millisecond;
-		System.out.println("게임 소요 시간 " + (float)millisecond/1000+ "초입니다.");
-		sc.close();
+		
+		
 	}
 }
